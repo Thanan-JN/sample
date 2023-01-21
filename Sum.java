c403cc9670a8fde2a6f506baaa4154fda874167e
Thanan-JN <107987443+Thanan-JN@users.noreply.github.com>
2023-01-21 11:34:00 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -3,6 +3,7 @@ class Sum{
         int a= 3;
         int b= 4;
         System.out.println(a+b);
+        System.out.println(a-b);
         // this is from local machine 1
         //this is a summation operation
         // from machine 2
